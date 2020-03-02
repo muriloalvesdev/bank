e7965ab95e8883201ac320cc8a6d17ce5605b5ab
muriloalvesdev <murilo.alves@idtrust.com.br>
2020-03-02 14:05:58 -0300

    version 1.0

---
+++ bank/src/main/java/br/com/bank/service/BankService.java
@@ -0,0 +1,15 @@
+package br.com.bank.service;
+
+import br.com.bank.domain.model.Bank;
+import br.com.bank.dto.BankDataTransferObject;
+import br.com.bank.dto.BankDataTransferObjectDeposit;
+
+public interface BankService {
+    void checkIfThereIsALimitOnTheCardAndDebitTheAvailableAmount(
+            BankDataTransferObject bankDTO);
+
+    Bank save(BankDataTransferObject bankDTO);
+
+    Bank depositValueValueInAccount(
+            BankDataTransferObjectDeposit bankDTODeposit);
+}
