31adc5b3858aa38d0c117fef83aa4f3bd8ec6daa
muriloalvesdev <murilo.alves@idtrust.com.br>
2020-03-02 16:59:42 -0300

    config bank

---
+++ src/main/java/br/com/bank/config/Config.java
@@ -0,0 +1,23 @@
+package br.com.bank.config;
+
+import java.math.BigDecimal;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import br.com.bank.domain.model.Bank;
+import br.com.bank.domain.repository.BankRepository;
+
+@Configuration
+public class Config {
+
+  @Autowired
+  private BankRepository repository;
+
+  @Bean
+  public void persistBank() {
+    repository.deleteAll();
+    repository
+        .saveAndFlush(new Bank("00001", "1234-x", BigDecimal.valueOf(876543456723345L), "NUBANK"));
+  }
+
+}
