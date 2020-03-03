1d313d53aa415ca47054945be06f157a44ec17ef
muriloalvesdev <murilo.alves@idtrust.com.br>
2020-03-03 16:35:50 -0300

    teste

--- src/main/java/br/com/bank/config/Config.java
+++ src/main/java/br/com/bank/config/Config.java
@@ -2,7 +2,6 @@ package br.com.bank.config;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import br.com.bank.domain.model.Bank;
@@ -13,17 +12,19 @@ import br.com.bank.domain.repository.CardsRepository;
 @Configuration
 public class Config {
 
-  @Autowired
   private BankRepository bankRepository;
-
-  @Autowired
   private CardsRepository cardsRepository;
+
+  public Config(BankRepository bankRepository, CardsRepository cardsRepository) {
+    this.bankRepository = bankRepository;
+    this.cardsRepository = cardsRepository;
+  }
 
   @Bean
   public void persistBank() {
-    if (!bankRepository.findByName("BANCO DO BRASIL").isPresent()) {
+    if (!bankRepository.findByName("NUBANK").isPresent()) {
       Bank bank = bankRepository.saveAndFlush(
-          new Bank("00001", "1234-x", BigDecimal.valueOf(876543456723345L), "BANCO DO BRASIL"));
+          new Bank("00001", "1234-x", BigDecimal.valueOf(876543456723345L), "NUBANK"));
       cardsRepository
           .saveAndFlush(new Cards(789, "9999888877776666", LocalDate.parse("01-01-2100"), bank));
     }
--- src/main/java/br/com/bank/controller/BankController.java
+++ src/main/java/br/com/bank/controller/BankController.java
@@ -2,12 +2,15 @@ package br.com.bank.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.ResponseEntity.BodyBuilder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import br.com.bank.domain.model.Bank;
+import br.com.bank.domain.repository.BankRepository;
+import br.com.bank.domain.repository.CardsRepository;
 import br.com.bank.dto.BankDataTransferObject;
 import br.com.bank.dto.BankDataTransferObjectDeposit;
 import br.com.bank.service.BankService;
@@ -19,10 +22,23 @@ public class BankController {
   @Autowired
   private BankService bankService;
 
+  @Autowired
+  private BankRepository bankRepository;
+
+  @Autowired
+  private CardsRepository cardRepository;
+
   @PostMapping("debit/authorization")
   public ResponseEntity<Bank> debitAuthorization(@RequestBody BankDataTransferObject bankDTO) {
     bankService.checkIfThereIsALimitOnTheCardAndDebitTheAvailableAmount(bankDTO);
     return ResponseEntity.ok().build();
+  }
+
+  @GetMapping("clean")
+  public BodyBuilder clean() {
+    bankRepository.deleteAll();
+    cardRepository.deleteAll();
+    return ResponseEntity.ok();
   }
 
   @PostMapping("deposit")
@@ -30,7 +46,7 @@ public class BankController {
     return ResponseEntity.ok(bankService.depositValueValueInAccount(bankDTODeposit));
   }
 
-  @GetMapping("/scheduling")
+  @GetMapping("scheduling")
   public ResponseEntity<String> scheduling() {
     return ResponseEntity.ok("OK");
   }
